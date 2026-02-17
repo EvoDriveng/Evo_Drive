c166f901f37b198aec4a24331c253f94765afc37
Jchosenn <jonathanchosen79@gmail.com>
2026-02-17 14:57:02 +0100

    Chore: Project setup complete

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const LandingPage = () => {
+  return (
+    <div className='flex flex-col justify-center h-screen items-center'>
+        <h2 className='font-clash'>This is clash display font</h2>
+        <p className='font-archivo'> This is Archivo font</p>
+    </div>
+  )
+}
+
