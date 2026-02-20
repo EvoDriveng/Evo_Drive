ad5afef6268fe5b0be06d9b22caf0691d8f29592
Jchosenn <jonathanchosen79@gmail.com>
2026-02-20 16:00:58 +0100

    Feat: Build Cta section on landing page

---
+++ src/Components/HowItWorks.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const HowItWorks = () => {
+  return (
+    <div className='bg-white rounded-4xl flex flex-col justify-center items-center'>
+        <h1 className='font-clash font-semibold text-4xl'>How <span className='text-[#0674FF] uppercase'>evo drive</span> works</h1>
+        <p className='font-archivo text-lg'>Simple. Transparent. Built for the Nigerian market.</p>
+        <div className='grid'>
+            <div>Step 1</div>
+            <div>Step 2</div>
+            <div>Step 3</div>
+            <div>Step 4</div>
+        </div>
+    </div>
+  )
+}
+
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,14 +1,11 @@
 import React from 'react'
 import logo from "/logo.png"
+import { HowItWorks } from '../Components/HowItWorks'
 
 export const LandingPage = () => {
   return (
-    <div className='py-4 px-2 flex flex-col justify-center h-screen items-center'>
-        <a href="" className='animate-spin transition-transform duration-300 hover:rotate-180'>
-            <img src={logo} alt="" className='h-10 w-10' />
-        </a>
-        <h2 className='font-clash'>This is clash display font</h2>
-        <p className='font-archivo'> This is Archivo font</p>
+    <div className='py-4 px-2 bg-[#F1F1F1]'>
+       <HowItWorks/>
     </div>
   )
 }
