063f59b00637d8af35d3d2dd03044fdc08522cd9
Jchosenn <jonathanchosen79@gmail.com>
2026-03-11 16:44:26 +0100

    Fix: Undo the comment on the footer section

--- src/pages/LandingPage/index.jsx
+++ src/pages/LandingPage/index.jsx
@@ -19,12 +19,12 @@ export const LandingPage = () => {
         <WhyPartner/>
         <Cta/>
       <ElectricVehicles />
-      <Footer />
-      {/* <a href="" className='animate-spin transition-transform duration-300 hover:rotate-180'>
+       <Footer />
+     {/*<a href="" className='animate-spin transition-transform duration-300 hover:rotate-180'>
             <img src={logo} alt="" className='h-10 w-10' />
         </a>
         <h2 className='font-clash'>This is clash display font</h2>
         <p className='font-archivo'> This is Archivo font</p> */}
     </div>
-  );
-};
+  )
+}
